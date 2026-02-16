32f18dd0c3f2ab2d261a5a1e759f7481cba7acc9
francesco oviedo <francesco.oviedo@defensoria.mg.def.br>
2026-02-16 08:40:30 -0300

    logo nova e mainfest

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from "@mui/material";
-
+import { Box, Button } from "@mui/material";
+import Image from "next/image";
 export default function Home() {
   return (
-    <div>
-   home
-    </div>
+   <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      <Image src="/assets/images/logo.png" alt="Logo" width={200} height={200} />
+      <Button variant="contained" color="primary" href="/criar-evento" sx={{ mt: 4 }}>
+        Criar Evento
+      </Button>
+    </Box>
   );
 }
